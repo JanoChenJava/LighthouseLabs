f537065eef8adde90a3859bed36e7b76ad540aba
JanoChenJava <jiananchen2014@gmail.com>
2017-03-25 14:49:52 -0400

    java
    workshop

---
+++ client/app.js
@@ -0,0 +1,13 @@
+var socket = io();
+
+$('form').submit(function(){
+ var text = $('#message').val();
+ socket.emit('message',text);
+    $('#message').val('');
+ return false;
+});
+
+socket.on('message', function(msg){
+    var incomingMessage = $('<li>').text(msg);
+    $('#messages').append(incomingMessage);
+});
