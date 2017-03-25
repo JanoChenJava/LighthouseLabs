94e4f51a71efe642a95065f8db5a2ac83ce33431
JanoChenJava <jiananchen2014@gmail.com>
2017-03-25 15:22:43 -0400

    fix some bugs
    fix some bugs

--- client/app.js
+++ client/app.js
@@ -1,7 +1,7 @@
 var socket = io();
 
 $('form').submit(function(){
- var text = $('#message').val();
+ var text = $('#initials').val() + " "+ "says" + ":" + $('#message').val();
  socket.emit('message',text);
     $('#message').val('');
  return false;
